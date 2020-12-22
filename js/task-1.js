e83e296ef0479b957b225648790c5987493a6099
yarik000315 <yarik000315@gmail.com>
2020-12-22 16:51:12 +0200

    add_js

---
+++ js/task-1.js
@@ -0,0 +1,9 @@
+const categories = document.querySelectorAll("#categories>.item");
+console.log(`в списку ${categories.length} категорії`);
+
+const nameOfCategories = document.querySelectorAll(".item>h2");
+const itemsOfCategories = document.querySelectorAll(".item>ul");
+
+function getOurCountOfItems (types, items) {
+    for (let i = 0; i < types.length; i++)
+}
